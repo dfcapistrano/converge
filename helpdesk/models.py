942af94ec2fefc7c574466449e35378b27a3bc9b
Davi Capistrano <dfcapistrano@gmail.com>
2019-04-03 15:55:00 -0400

    commit

--- helpdesk/models.py
+++ helpdesk/models.py
@@ -1,12 +1,3 @@
-"""
-django-helpdesk - A Django powered ticket tracker for small enterprise.
-
-(c) Copyright 2008 Jutda. All Rights Reserved. See LICENSE for details.
-
-models.py - Model (and hence database) definitions. This is the core of the
-            helpdesk structure.
-"""
-
 from __future__ import unicode_literals
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
